0683ea91dbfa51261b56b2e1c1d32ba89eb44d58
I_Agreed <48776053+I-Agreed@users.noreply.github.com>
2020-05-06 17:42:00 +1000

    added arduino code

---
+++ arduino/mouse.ino
@@ -0,0 +1,23 @@
+float x;
+float y;
+
+void setup() {
+  Serial.begin(9600);
+  
+
+}
+
+void loop() {
+  x = analogRead(5);
+  x -= 512.5;
+  x /= 512.5;
+  
+  y = analogRead(4);
+  y -= 512.5;
+  y /= 512.5;
+  Serial.print(x);
+  Serial.print(", ");
+  Serial.print(y);
+  Serial.println();
+
+}
